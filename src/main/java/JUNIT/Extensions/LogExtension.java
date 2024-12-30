5d8815b2d5cfc729ef7f197f46c7fb1ddf9ef22f
tzippykovalsky <9098537@gmail.com>
2024-12-30 21:31:04 +0200

    Add LogExtension to handle logging and ensure logs are saved in 'src/test/resources/logs' folder

--- src/main/java/JUNIT/Extensions/LogExtension.java
+++ src/main/java/JUNIT/Extensions/LogExtension.java
@@ -7,23 +7,30 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.File;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LogExtension implements BeforeAllCallback, AfterAllCallback {
 
     private static BufferedWriter logWriter;
+    private static final String LOG_DIRECTORY = "src/test/resources/logs";
+    private static final String LOG_FILE = LOG_DIRECTORY + "/test-log.txt";
 
-    // אתחול הלוג לפני כל הבדיקות
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
+        // ודא שהתיקיה קיימת ואם לא, צור אותה
+        File logDir = new File(LOG_DIRECTORY);
+        if (!logDir.exists()) {
+            logDir.mkdirs();  // יצירת תיקיה אם היא לא קיימת
+        }
+
         // יצירת קובץ לוג או פתיחה של קובץ קיים
-        logWriter = new BufferedWriter(new FileWriter("test-log.txt", true));
+        logWriter = new BufferedWriter(new FileWriter(LOG_FILE, true));
         logWriter.write(getCurrentTimestamp() + " התחלת הבדיקות- \n" + context.getDisplayName() + "\n");
         logWriter.flush();
     }
 
-    // ניקוי הלוג אחרי כל הבדיקות
     @Override
     public void afterAll(ExtensionContext context) throws Exception {
         if (logWriter != null) {
--- src/main/java/JUNIT/base/BaseTest.java
+++ src/main/java/JUNIT/base/BaseTest.java
@@ -1,12 +1,16 @@
 package JUNIT.base;
 
+import JUNIT.Extensions.LogExtension;
+import JUNIT.Extensions.WatcherExtension;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
 import store.TestContext;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import selenium.driver.DriverManager;
 import selenium.driver.WebDriverFactory;
 
+@ExtendWith({WatcherExtension.class, LogExtension.class})
 public abstract class BaseTest {
     protected DriverManager driver;
     protected String browserName;
